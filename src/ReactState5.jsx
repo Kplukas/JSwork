a1747d99764d50183a166d778610af14c45dc5d5
Kplukas <p.kristupas.p@gmail.com>
2022-12-01 00:13:48 +0200

    ReactState task 5 (initial commit)

---
+++ src/ReactState5.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './App.scss';
+import randNum from './functions/randNum';
+import Sq from './components/Sq';
+
+function App() {
+
+    const[sq, setSq] = useState([]);
+
+    const addBlue = () => {
+        setSq(s => [...s, { id: randNum(100000, 999999), bg: 'blue'}]);
+    }
+    const addRed = () => {
+        setSq(s => [...s, { id: randNum(100000, 999999), bg: 'red'}]);
+    }
+    const reset = () => {
+        const nulis = 0;
+        setSq(nulis);
+    }
+    return (
+        <div className="App">
+            <div className="App-header">
+                    <div className="bin">
+                    {
+                        sq?.map((sq) => <Sq key={sq.id} sq={sq} className="sq {sq.bg}" style={{backgroundColor: '{sq.bg}'}}/>)
+                    }
+                </div>
+                <div>
+                    <button onClick={addBlue}>ADD Blue</button>
+                    <button onClick={addRed}>ADD Red</button>
+                    <button onClick={reset}>RESET</button>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './ReactState4';
+import App from './ReactState5';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
