bd206293f3d6024a170828d78f9c5c71bc31803d
Kplukas <p.kristupas.p@gmail.com>
2022-12-04 18:42:01 +0200

    ReactBaseList task 4 update

--- src/ReactBaseList4.jsx
+++ src/ReactBaseList4.jsx
@@ -1,28 +1,19 @@
 import './App.scss';
 
+
 function App() {
-    const dogs = [
-        'šuo',
-        'šunius',
-        'Bobikas',
-        'kudlius',
-        'Šarikas',
-        'avigalvis',
-    ];
-    function isUpperCase(input) {
-        return input === String(input).toUpperCase();
-    }
-    for (let i = 0; i < dogs.length; i++) {
-        if (isUpperCase(dogs)) {
-            return (
-                <div className="App">
-                    <div className="App-header">
-                        {dogs.map(dog => <div className="cage" key={i}>{dog}</div>)}
-                    </div>
-                </div>
-            );
-        }
-    }
+
+    const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
+    function isUpperCase (input) {  
+        return input === String(input).toUpperCase()
+    };
+    return (
+        <div className="App">
+            <div className="App-header">
+                {dogs.map((dog, i) => isUpperCase(dog) === true  ? null : <div className='cage' key={i}>{dog}</div>)}   
+            </div>
+        </div>
+    );
 }
 
 export default App;
